3da1fdefd518ef8e16f198d606e1202d86e4175b
Allan Aleluia <allan.souza92@hotmail.com>
2024-01-30 19:49:09 +0000

    Conclusão do segundo projeto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -13,18 +13,23 @@ namespace DesafioProjetoHospedagem.Models
             DiasReservados = diasReservados;
         }
 
+        public class CapacidadeExcedida : Exception
+        {
+            public CapacidadeExcedida() : base("A capacidade da suíte foi excedida") { }
+        }
+
+
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                
+                throw new CapacidadeExcedida();
             }
         }
 
@@ -35,23 +40,26 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            
+            int totalHospedes = Hospedes.Count;
+            return totalHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+            
+            
             decimal valor = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+           
+
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = 0.9m;
+                valor = valor * desconto;
             }
 
             return valor;
